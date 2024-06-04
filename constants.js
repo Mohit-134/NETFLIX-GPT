4ebb950dd163f09c33d24ec9da335da09877dcfa
Mohit-134 <109890852+Mohit-134@users.noreply.github.com>
2024-06-04 15:40:23 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,41 @@
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+
+export const USER_AVATAR =
+  "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+
+  export const BG_URL =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+
+  export const URL = 'https://api.themoviedb.org/3/movie/now_playing?page=1';
+  export const POPULAR_URL = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+  export const TOP_RATED_URL = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+  export const UPCOMING_URL = 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1';
+
+
+  export const API_OPTIONS =  {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MzZjZmE2ODRlOGExZGFkY2U0YmZiYTZiMzhhMTg5NCIsInN1YiI6IjY1MjI3NjY4MGNiMzM1MTZmZWMzNGVjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8hWfEyfXkwmG6oaFWoGwVkbKNE_cKvhznkt0p-zKMTI'
+      
+    }
+  };
+
+
+
+
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
+
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+];
+
+export const OPENAI_KEY = "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";
